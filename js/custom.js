e7ec2b6f06890938be40912957830e3c02d835aa
lakedas <49954951+lakedas@users.noreply.github.com>
2019-05-08 21:14:57 -0300

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,13 +1,21 @@
 window.addEventListener("load", function(){
-	var nodes = document.querySelectorAll(".plain-header h2");
-	for(var i=0; i<nodes.length; i++){
-		var words = nodes[i].innerText;
-		var html = "";
-		for(var i2=0; i2<words.length; i2++){
-			if(words[i2] == " ")
-				html += words[i2];
-				else html += "<span>"+words[i2]+"</span>"
-		}
-		nodes[i].innerHTML = html;
+	var div = document.querySelectorAll(".plain-header h2");
+	letters = div.textContent.split('');
+	while(div.hasChildNodes()) div.removeChild(div.firstChild);
+
+	for(var i = 0; i < letters.length; i++) {
+	  var letter = document.createElement('span'),
+	      style = 'opacity ' + (Math.random() * 5 + 1) + 's linear';
+	  letter.appendChild(document.createTextNode(letters[i]));
+		letter.style.WebKitTransition = letter.style.MozTransition = 
+	    letter.style.MSTransition = letter.style.OTransition = letter.style.transition = style;
+	  letter.style.opacity = 0;
+	  div.appendChild(letter);
 	}
+
+	setTimeout(function() {
+	  for(var i = 0; i < div.childNodes.length; i++) {
+	    div.childNodes[i].style.opacity = 1;
+	  }
+	}, 0);
 });
