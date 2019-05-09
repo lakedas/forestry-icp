c91652e10f09114e74062ff159b85429290ec038
lakedas <49954951+lakedas@users.noreply.github.com>
2019-05-08 21:05:46 -0300

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,14 +1,13 @@
 window.addEventListener("load", function(){
 	var nodes = document.querySelectorAll(".plain-header h2");
 	for(var i=0; i<nodes.length; i++){
-	} 
+		var words = nodes[i].innerText;
+		var html = "";
+		for(var i2=0; i2<words.length; i2++){
+			if(words[i2] == " ")
+				html += words[i2];
+				else html += "<span>"+words[i2]+"</span>"
+		}
+		nodes[i].innerHTML = html;
+	}
 });
-var words = nodes[i].innerText;
-var html = "";
-for(var i2=0; i2<words.length; i2++){
-	if(words[i2] == " ")html += 
-words[i2];
-		else html += 
-"<span>"+words[i2]+"</span>"
-}
-nodes[i].innerHTML = html;
